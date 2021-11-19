25a053899bdc9fb0ff54155161e10569728b9295
Ubuntu <ubuntu@ip-172-31-89-209.ec2.internal>
2021-11-19 18:49:13 +0000

    added log file

---
+++ Log.swift
@@ -0,0 +1,29 @@
+//
+//  Logs.swift
+//
+//  Created by Roman Cernetchi
+//  Created on 2021-11-19
+//  Version 1.0
+//  Copyright (c) 2020 Roman Cernetchi. All rights reserved.
+//
+//  This program calculates the amount of logs than can be put in a truck. 
+//
+// Variable Declaration
+let maxWeight: Float = 1100
+let logWeightPerMeter: Float = 20
+var heightOfLog: Float
+var maxAmountOfLogs: Float
+
+// User prompt
+print("Enter the log length (0.25, 0.5 or 1 meters): ", terminator: "")
+
+// Gathers the user input and converts it to Float
+heightOfLog = Float(readLine()!)!
+
+// Calculates the max amount of logs that can fit in the truck
+maxAmountOfLogs = maxWeight / logWeightPerMeter / heightOfLog
+
+// Outputs the result of the calculation to the user
+print("The truck can hold \(maxAmountOfLogs) logs.")
+
+print("\nDone.")
